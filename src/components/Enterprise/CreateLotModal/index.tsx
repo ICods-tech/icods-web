306f1b78fde9ed70330788518b091c6c644d5f15
Lucas <lucasmsea@outlook.com>
2022-03-15 22:20:50 -0300

    styles: finish structuring create qr codes modal

--- src/components/Enterprise/CreateLotModal/index.tsx
+++ src/components/Enterprise/CreateLotModal/index.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from 'react'
 import Modal from 'react-modal'
-import { TopModalContainer, TopModalTitleText } from './styles'
+import { Option } from 'react-dropdown'
+import 'react-dropdown/style.css'
+import { AddUser } from 'react-iconly'
+import {
+  Container,
+  ClientDropdown,
+  ClientLabel,
+  CloseIcon,
+  MidModalContainer,
+  QRCodeQuantityInput,
+  RegisterClientContainer,
+  RegisterClientText,
+  TopModalContainer,
+  TopModalTitleText,
+  GrayLine,
+  ButtonsContainer,
+  FooterContainer,
+  CancelButton,
+  CreateQRCodesButton,
+} from './styles'
 
 const CreateLotModal = ({ createLotModalOpen, closeModal }) => {
   const customStyles = {
@@ -14,8 +32,15 @@ const CreateLotModal = ({ createLotModalOpen, closeModal }) => {
       bottom: 'auto',
       marginRight: '-50%',
       transform: 'translate(-50%, -50%)',
+      borderWidth: 1,
+      padding: 0,
     },
   }
+  const MIN_VALUE = 1,
+    MAX_VALUE = 1000
+  const options = ['one', 'two', 'three']
+  const defaultOption = options[0]
+  const [selectedOption, setSelectedOption] = useState<Option | string>(defaultOption)
 
   return (
     <Modal
@@ -24,9 +49,42 @@ const CreateLotModal = ({ createLotModalOpen, closeModal }) => {
       style={customStyles}
       contentLabel="Example Modal"
     >
-      <TopModalContainer>
-        <TopModalTitleText>Criação de QR Codes</TopModalTitleText>
-      </TopModalContainer>
+      <Container>
+        <TopModalContainer>
+          <TopModalTitleText>Criação de QR Codes</TopModalTitleText>
+          <CloseIcon
+            onClick={closeModal}
+            src="/images/enterprise/close-icon.svg"
+            alt="Close icon"
+          />
+        </TopModalContainer>
+        <MidModalContainer>
+          <ClientLabel>Cliente:</ClientLabel>
+          <ClientDropdown
+            options={options}
+            onChange={(option: Option) => setSelectedOption(option)}
+            value={defaultOption}
+            placeholder="Select an option"
+          />
+          <RegisterClientContainer>
+            <AddUser set="light" size={14} primaryColor="var(--background)" />
+            <RegisterClientText>Cadastre um cliente aqui</RegisterClientText>
+          </RegisterClientContainer>
+          <ClientLabel>Quantidade de QR Codes:</ClientLabel>
+          <QRCodeQuantityInput
+            onChange={
+              (e) => {}
+              // Ajustar boundaries
+            }
+          />
+        </MidModalContainer>
+        <FooterContainer>
+          <ButtonsContainer>
+            <CancelButton onClick={closeModal}>Cancelar</CancelButton>
+            <CreateQRCodesButton>Criar QR Codes</CreateQRCodesButton>
+          </ButtonsContainer>
+        </FooterContainer>
+      </Container>
     </Modal>
   )
 }
--- src/components/Enterprise/CreateLotModal/styles.ts
+++ src/components/Enterprise/CreateLotModal/styles.ts
@@ -1,9 +1,17 @@
 import styled from 'styled-components'
+import Dropdown, { Option } from 'react-dropdown'
+
+export const Container = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+`
 
 export const TopModalContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  margin: 1.5rem;
 `
 
 export const TopModalTitleText = styled.h1`
@@ -13,4 +21,106 @@ export const TopModalTitleText = styled.h1`
   font-size: 24px;
   line-height: 33px;
   color: var(--text-title);
+  width: 90%;
+`
+
+export const CloseIcon = styled.img`
+  cursor: pointer;
+`
+
+export const MidModalContainer = styled.div`
+  margin-top: 4rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const ClientLabel = styled.h3`
+  font-family: Manrope;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 22px;
+  color: var(--text-title);
+`
+
+export const ClientDropdown = styled(Dropdown)`
+  margin-top: 1rem;
+  width: 17.5rem;
+`
+
+export const RegisterClientContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 0.5rem;
+  margin-bottom: 2rem;
+`
+
+export const RegisterClientText = styled.h3`
+  font-family: Manrope;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 0.875rem;
+  line-height: 1.5rem;
+  color: var(--background);
+  margin-left: 0.5rem;
+`
+
+export const QRCodeQuantityInput = styled.input.attrs({
+  type: 'number',
+  max: '1000',
+  min: '1',
+})`
+  margin-top: 0.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 4.5rem;
+  height: 1.875rem;
+  text-align: center;
+`
+
+export const FooterContainer = styled.div`
+  height: 5.625rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  border-top: 0.5px solid rgba(0, 0, 0, 0.17);
+  margin-top: auto;
+  margin-bottom: 0;
+  padding-right: 1.5rem;
+  width: 100%;
+`
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+export const GrayLine = styled.div`
+  width: 32.6875rem;
+  height: 0.0313rem;
+  background: rgba(0, 0, 0, 0.17);
+`
+
+export const CancelButton = styled.button`
+  width: 9.5rem;
+  height: 2.5rem;
+  font-family: Manrope;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 24px;
+  color: var(--shape);
+  background: #5f6368;
+  border-radius: 2px;
+  border: 0;
+`
+
+export const CreateQRCodesButton = styled(CancelButton)`
+  background: var(--background);
+  margin-left: 1.125rem;
 `
