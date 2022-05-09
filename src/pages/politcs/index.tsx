fce82c7e3f74c10e044b713e4c9643242e829df0
Thomas <thomasaraujo@mat.ci.ufpb.br>
2022-05-09 19:54:43 -0300

    feat: adjust terms and add politcs page

--- src/pages/politcs/index.tsx
+++ src/pages/politcs/index.tsx
@@ -5,7 +5,7 @@ import { Header } from "../../components/Enterprise/Header"
 import { Footer } from '../../components/Footer'
 import { ChangesTermsConditions, ContactUs, ContainerBody, ContentContainer, Paragraph, SectionContainer, Subtitle, TermsConditions } from '../../pageStyles/termsStyles'
 
-const Terms = () => {
+const Politcs = () => {
   return (
     <>
       <GlobalStyle />
@@ -15,18 +15,19 @@ const Terms = () => {
       <Header darkMode={false} />
       <ContentContainer>
         <SectionContainer>
-          <h2> Termos de Serviço </h2>
+          <h2> Políticas de privacidade </h2>
         </SectionContainer>
+
         <ContainerBody>
           <TermsConditions>
             <Subtitle>
-              Terms & Conditions
+              Last updated: November 15, 2021
             </Subtitle>
             <Paragraph>
-              By downloading or using the app, these terms will automatically apply to you – you should make sure therefore that you read them carefully before using the app. You’re not allowed to copy, or modify the app, any part of the app, or our trademarks in any way. You’re not allowed to attempt to extract the source code of the app, and you also shouldn’t try to translate the app into other languages, or make derivative versions. The app itself, and all the trade marks, copyright, database rights and other intellectual property rights related to it, still belong to iCods.
+              This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.
             </Paragraph>
             <Paragraph>
-              iCods is committed to ensuring that the app is as useful and efficient as possible. For that reason, we reserve the right to make changes to the app or to charge for its services, at any time and for any reason. We will never charge you for the app or its services without making it very clear to you exactly what you’re paying for.
+              We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the Privacy Policy Generator.
             </Paragraph>
             <Paragraph>
               The iCods app stores and processes personal data that you have provided to us, in order to provide our Service. It’s your responsibility to keep your phone and access to the app secure. We therefore recommend that you do not jailbreak or root your phone, which is the process of removing software restrictions and limitations imposed by the official operating system of your device. It could make your phone vulnerable to malware/viruses/malicious programs, compromise your phone’s security features and it could mean that the iCods app won’t work properly or at all.
@@ -89,5 +90,5 @@ const Terms = () => {
 
 
 
-export default Terms
+export default Politcs
 
